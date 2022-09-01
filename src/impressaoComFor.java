e40b4974732f68991ee6636105cdfb491203bae5
Karen Almeida <karenalmeida61@hotmail.com>
2022-09-01 18:36:25 -0300

    Apagando varias desnecessarias do exercicio 5

--- src/impressaoComFor.java
+++ src/impressaoComFor.java
@@ -1,7 +1,7 @@
 public class impressaoComFor {
     public static void main(String[] args) {
-        int soma = 0;
         for (int i = 1; i <= 100; i++)
+            
             System.out.println(i);
 
     }
--- src/impressaoComWhile.java
+++ src/impressaoComWhile.java
@@ -1,6 +1,6 @@
 public class impressaoComWhile {
     public static void main(String[] args) {
-        int soma = 0;
+
         int i = 0;
         while (i < 100) {
             i++;
