1f9fff3258912d9a66315cb686b2cfe8bcbc6d1c
Karen Almeida <karenalmeida61@hotmail.com>
2022-09-01 17:22:28 -0300

    Adicão da ultima parte do exercicio 4, utilizando while para calcular e imprimir

---
+++ src/impressaoComWhile.java
@@ -0,0 +1,12 @@
+public class impressaoComWhile {
+    public static void main(String[] args) {
+        int soma=0;
+        int i=0;
+        while (i<=100){
+            soma+=i;
+            i++;
+
+        }
+        System.out.println("A soma dos 100 primeiros números é " + soma);
+    }
+}
