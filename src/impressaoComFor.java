f0bfee58ba292494ee12b9e1cc684d25cdf4f7e5
Karen Almeida <karenalmeida61@hotmail.com>
2022-09-01 17:16:38 -0300

    Inclusão de parte do exercicio 4(soma utilizando for)

---
+++ src/impressaoComFor.java
@@ -0,0 +1,8 @@
+public class impressaoComFor {
+    public static void main(String[] args) {
+        int soma=0;
+       for(int i=1;i<=100;i++)
+       soma +=i;
+        System.out.println("A soma dos 100 primeiros números é " + soma);
+    }
+}
