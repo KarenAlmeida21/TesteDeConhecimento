dc3c57ccc14f382fa4219617b0a78c43dd97a6bf
Karen Almeida <karenalmeida61@hotmail.com>
2022-09-01 18:29:18 -0300

    Correção de parte do exercicio 5 para respeitar a regra de negocio

--- src/impressaoComWhile.java
+++ src/impressaoComWhile.java
@@ -2,12 +2,12 @@ public class impressaoComWhile {
     public static void main(String[] args) {
         int soma = 0;
         int i = 0;
-        while (i <= 100) {
-            soma += i;
+        while (i < 100) {
             i++;
+            System.out.println(i);
 
         }
 
-        System.out.println("A soma dos 100 primeiros números é " + soma);
+
     }
 }
