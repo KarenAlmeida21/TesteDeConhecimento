537a9e405ceb429f4b1cb2b5a4ecc2c6bc95d186
Karen Almeida <karenalmeida61@hotmail.com>
2022-09-03 17:25:59 -0300

    método abstrato para calcula area

--- src/calculoFormas/FiguraGeometrica.java
+++ src/calculoFormas/FiguraGeometrica.java
@@ -2,5 +2,23 @@ package calculoFormas;
 
 public abstract class FiguraGeometrica {
     private double area;
-    //public tipoDeFigura tipoDeFigura;
+    public TipoDeFigura tipoDeFigura;
+
+    public FiguraGeometrica(calculoFormas.TipoDeFigura tipoDeFigura) {
+        this.area = area;
+        this.tipoDeFigura = tipoDeFigura;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public void setArea(double area) {
+        this.area = area;
+    }
+
+    public TipoDeFigura getTipoDeFigura() {
+        return tipoDeFigura;
+    }
+    public abstract double calcularArea(double medida);
 }
