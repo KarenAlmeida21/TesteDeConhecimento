ac9284b1589104f80db1abbf9aacdd8ac4967ee6
Karen Almeida <karenalmeida61@hotmail.com>
2022-09-03 17:32:31 -0300

    Sobrescrever método alterarFigura

--- src/calculoFormas/Quadrado.java
+++ src/calculoFormas/Quadrado.java
@@ -3,9 +3,17 @@ package calculoFormas;
 public class Quadrado extends FiguraGeometrica{
     private double lado;
 
+    public Quadrado(TipoDeFigura tipoDeFigura) {
+        super(tipoDeFigura);
+    }
+
     @Override
     public double calcularArea(double lado) {
         setArea(lado * lado);
         return getArea();
+    }
+    @Override
+    public void alterarFigura(calculoFormas.TipoDeFigura tipoDeFigura) {
+        super.alterarFigura(TipoDeFigura.QUADRADO);
     }
 }
