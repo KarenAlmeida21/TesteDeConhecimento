7deab82eaa82fd4ff2eb79c08d8047a392ac2bfe
Karen Almeida <karenalmeida61@hotmail.com>
2022-09-05 14:09:29 -0300

    Adicionar método que cadastra Produtos

--- src/setimoExercicio/Estoque.java
+++ src/setimoExercicio/Estoque.java
@@ -1,7 +1,26 @@
 package setimoExercicio;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Estoque {
-    private List<Produto> produtos;
+    private static List<Produto> produtosList = new ArrayList<>();
+
+    public Estoque() {
+    }
+
+    public List<Produto> getProdutosList() {
+        return produtosList;
+    }
+
+    public void setProdutosList(List<Produto> produtosList) {
+        this.produtosList = produtosList;
+    }
+
+    public static Produto cadastrarProduto(String nome, double valor){
+        Produto produto = new Produto(nome,valor);
+        produtosList.add(produto);
+        return produto;
+    }
+
 }
--- src/setimoExercicio/Produto.java
+++ src/setimoExercicio/Produto.java
@@ -4,7 +4,9 @@ public class Produto {
     private String nome;
     private double valor;
 
-    public Produto() {
+    public Produto(String nome, double valor) {
+        this.nome = nome;
+        this.valor = valor;
     }
 
     public String getNome() {
