a04fae5d937c825e27ad9ff85b3379d68f039d78
Karen Almeida <karenalmeida61@hotmail.com>
2022-09-05 17:55:20 -0300

    Adicionando exceptions

--- src/setimoExercicio/Estoque.java
+++ src/setimoExercicio/Estoque.java
@@ -1,7 +1,5 @@
 package setimoExercicio;
 
-import org.w3c.dom.ls.LSOutput;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,52 +9,58 @@ public class Estoque {
     public Estoque() {
     }
 
-    public List<Produto> getProdutosList() {
-        return produtosList;
-    }
-
-    public void setProdutosList(List<Produto> produtosList) {
-        this.produtosList = produtosList;
-    }
-
     public static Produto cadastrarProduto(String nome, double valor) throws Exception {
         verificarCadastroProdutoPositivo(nome);
         Produto produto = new Produto(nome, valor);
         produtosList.add(produto);
+        Sistema.informarProdutoCadastradoComSucesso();
         return produto;
     }
 
     public static void exibir() {
         for (Produto produtoReferencia : produtosList) {
             System.out.println(produtoReferencia.getNome());
+            System.out.println(produtoReferencia.getValor());
 
         }
 
     }
 
-
-
-   public  static void deletarProduto(String nome) throws Exception {
+    public static void deletarProduto(String nome) throws Exception {
         for (Produto produto : produtosList) {
             if (produto.getNome().equalsIgnoreCase(nome)) {
                 produtosList.remove(produto);
-                System.out.println("Produto deletado");
+                throw new Exception("Deletado com sucesso \n");
+
+
             }
         }
+        Sistema.mensagemErroNaDeleção();
 
     }
-    public static  void verificacaoListaVazia(){
-        if(Estoque.produtosList.size()==0){
-            System.out.println("Não há produtos cadastrados");
+
+    public static void verificacaoListaVazia() {
+        if (Estoque.produtosList.size() == 0) {
+            System.out.println("Não há produtos cadastrados\n");
         }
     }
 
     public static void verificarCadastroProdutoPositivo(String nome) throws Exception {
         for (Produto produto : produtosList) {
             if (produto.getNome().equalsIgnoreCase(nome)) {
-                throw new Exception("Produto já cadastrado. Verifique!");
+                throw new Exception("Produto já cadastrado. Verifique!\n");
             }
         }
     }
 
+    public static void verificarCadastroProdutoNegativo(String nome) throws Exception {
+        for (Produto produto : produtosList) {
+            if (!produto.getNome().equalsIgnoreCase(nome)) {
+                throw new Exception("Produto não encontrado \n");
+            }
+        }
+    }
+
+
 }
+
--- src/setimoExercicio/Sistema.java
+++ src/setimoExercicio/Sistema.java
@@ -12,6 +12,18 @@ public class Sistema {
         System.out.println("Digite uma opção existente no menu\n");
     }
 
+    public static void redirecionarMenu() {
+        System.out.println("Você será redirecionado para o menu anterior\n");
+
+    }
+    public static void mensagemErroNaDeleção(){
+        System.out.println("Não existe este produto no estoque");
+    }
+
+    public static void informarProdutoCadastradoComSucesso() {
+        System.out.println("Proguto cadastrado com sucesso!\n ");
+    }
+
     public static Produto cadastrarProduto() throws Exception {
         String nomeProduto = obterDados("Digite o nome do produto que será cadastrado").nextLine();
         Double valorProduto = obterDados("Digite o valor do produto que será cadastrado").nextDouble();
@@ -20,59 +32,77 @@ public class Sistema {
 
     public static void deletarProduto() throws Exception {
         String nomeProduto = obterDados("Digite o nome do produto que será deletado").nextLine();
-         Estoque.deletarProduto(nomeProduto);
+        Estoque.deletarProduto(nomeProduto);
     }
 
     public static void exibirProdutos() {
         Estoque.exibir();
-
     }
 
-    public static void mensagemEncerramento(){
+    public static void mensagemEncerramento() {
         System.out.println("Sistema encerrado\n");
     }
 
-
-
-    public static void menuInicial(){
-        System.out.println("ESTOQUE");
+    public static void menuInicial() {
+        System.out.println("\nESTOQUE\n");
         System.out.println("Digite 1 para cadastrar produto");
         System.out.println("Digite 2 para deletar produto");
         System.out.println("Digite 3 para exibir produto");
         System.out.println("Digite 4 para sair do sistema de estoque");
     }
 
+    public static void verificacaoCadastrarNovamente() throws Exception {
+        boolean loop = true;
+        while (loop) {
+            int cadastrarMaisProdutos = obterDados("Deseja cadastrar mais produtos:\n" +
+                    "1 para Sim\n" +
+                    "2 para não\n").nextInt();
+
+            if (cadastrarMaisProdutos == 1) {
+                cadastrarProduto();
+            }
+            if (cadastrarMaisProdutos == 2) {
+                loop = false;
+                redirecionarMenu();
+            }
+        }
+
+    }
+
     public static boolean operacaoUsuario() throws Exception {
-        boolean menu= true;
+        boolean menu = true;
 
         menuInicial();
         int opcaoUsuario = obterDados("Digite a opção desejada:").nextInt();
-        while (menu){
-            if(opcaoUsuario == 1){
+        while (menu) {
+            if (opcaoUsuario == 1) {
+
                 cadastrarProduto();
+
+                verificacaoCadastrarNovamente();
                 break;
             }
-            if(opcaoUsuario==2 ){
+            if (opcaoUsuario == 2) {
                 deletarProduto();
                 break;
             }
-            if(opcaoUsuario == 3){
+            if (opcaoUsuario == 3) {
                 Estoque.verificacaoListaVazia();
                 exibirProdutos();
                 break;
             }
-            if(opcaoUsuario == 4){
+            if (opcaoUsuario == 4) {
                 mensagemEncerramento();
-                menu=false;
+                menu = false;
                 break;
-            }else {
+            } else {
                 opcaoInvalida();
                 break;
             }
 
-            }
+        }
         return menu;
     }
-    }
+}
 
 
