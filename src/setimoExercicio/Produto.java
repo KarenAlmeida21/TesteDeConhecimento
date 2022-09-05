9dc5c8f50f6b981784215b22ff198974c5d4febf
Karen Almeida <karenalmeida61@hotmail.com>
2022-09-05 13:46:22 -0300

    Adicionando atributos, construtor e getters e setters a classe Produto

--- src/setimoExercicio/Produto.java
+++ src/setimoExercicio/Produto.java
@@ -1,4 +1,25 @@
 package setimoExercicio;
 
 public class Produto {
+    private String nome;
+    private double valor;
+
+    public Produto() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
 }
